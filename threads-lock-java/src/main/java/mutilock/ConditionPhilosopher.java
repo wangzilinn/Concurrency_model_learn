333dd0e25e19acbc3fed2a51265927212c4e1a17
wangzilin <wangzilinn@gmail.com>
2021-09-10 20:46:18 +0800

    条件变量及原子变量

--- threads-lock-java/src/main/java/mutilock/ConditionPhilosopher.java
+++ threads-lock-java/src/main/java/mutilock/ConditionPhilosopher.java
@@ -1,9 +1,104 @@
 package mutilock;
 
+import java.util.Random;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @Author: wangzilinn@gmail.com
  * @Date: 9/9/2021 9:56 PM
  */
 public class ConditionPhilosopher extends Thread {
+    final private int id;
+    private boolean eating;
+    private ConditionPhilosopher leftPhilosopher;
+    private ConditionPhilosopher rightPhilosopher;
+    final private ReentrantLock table;
+    final private Condition condition;
 
+    public ConditionPhilosopher(int id, ReentrantLock table) {
+        this.id = id;
+        this.eating = false;
+        this.table = table;
+        this.condition = table.newCondition();
+    }
+
+    public void setLeftPhilosopher(ConditionPhilosopher leftPhilosopher) {
+        this.leftPhilosopher = leftPhilosopher;
+    }
+
+    public void setRightPhilosopher(ConditionPhilosopher rightPhilosopher) {
+        this.rightPhilosopher = rightPhilosopher;
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                think();
+                eat();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void think() throws InterruptedException {
+        // 先锁住桌子，此时只能有一个人操作
+        table.lock();
+        try{
+            eating = false;
+            // 放下左边筷子，通知左边
+            leftPhilosopher.condition.signal();
+            // 放下右边筷子，通知右边
+            rightPhilosopher.condition.signal();
+        }finally{
+            // 解锁桌子
+            table.unlock();
+        }
+        System.out.println(id + " is thinking");
+        Thread.sleep(1000);
+    }
+
+    private void eat() throws InterruptedException {
+        // 先锁住桌子，此时只能由一个人操作
+        table.lock();
+        try {
+            // 如果左边或者右边在吃饭
+            while (leftPhilosopher.eating || rightPhilosopher.eating) {
+                // 则释放锁，并原地阻塞，如果左边或右边的人发了signal，则恢复执行
+                condition.await();
+            }
+            System.out.println(id + " is eating");
+            eating = true;
+        } finally {
+            table.unlock();
+        }
+        Thread.sleep(1000);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReentrantLock table = new ReentrantLock();
+        ConditionPhilosopher philosopher1 = new ConditionPhilosopher(1, table);
+        ConditionPhilosopher philosopher2 = new ConditionPhilosopher(2, table);
+        ConditionPhilosopher philosopher3 = new ConditionPhilosopher(3, table);
+
+        philosopher1.setLeftPhilosopher(philosopher3);
+        philosopher1.setRightPhilosopher(philosopher2);
+
+        philosopher2.setLeftPhilosopher(philosopher1);
+        philosopher2.setRightPhilosopher(philosopher3);
+
+        philosopher3.setLeftPhilosopher(philosopher2);
+        philosopher3.setRightPhilosopher(philosopher1);
+
+        philosopher1.start();
+        philosopher2.start();
+        philosopher3.start();
+
+        philosopher1.join();
+        philosopher2.join();
+        philosopher3.join();
+    }
 }
--- threads-lock-java/src/main/java/race/condition/Counting.java
+++ threads-lock-java/src/main/java/race/condition/Counting.java
@@ -1,5 +1,7 @@
 package race.condition;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Author: wangzilinn@gmail.com
  * @Date: 9/8/2021 8:59 PM
@@ -9,29 +11,32 @@ public class Counting {
         class Counter {
             private int count = 0;
 
+            private int concurrentCount = 0;
+
+            private final AtomicInteger atomicCount = new AtomicInteger();
+
             // synchronized 来避免加法错误
-            public synchronized void increment() {
-                ++count;
+            public synchronized void concurrentIncrement() {
+                ++concurrentCount;
             }
 
-            public int getCount() {
-                return count;
+            public void increment() {
+                ++count;
+                atomicCount.incrementAndGet();
             }
         }
 
         Counter counter = new Counter();
 
-        class CounterThread extends Thread {
-            @Override
-            public void run() {
-                for (int i = 0; i < 1000; i++) {
-                    counter.increment();
-                }
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.concurrentIncrement();
+                counter.increment();
             }
-        }
+        };
 
-        CounterThread thread1 = new CounterThread();
-        CounterThread thread2 = new CounterThread();
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
         long startTime = System.nanoTime();
         thread1.start();
         thread2.start();
@@ -40,8 +45,9 @@ public class Counting {
         thread2.join();
         long endTime = System.nanoTime();
         System.out.println("程序运行时间： " + (endTime - startTime) + "ns");
-        System.out.println(counter.getCount());
         // 私有属性也能直接访问
+        System.out.println(counter.concurrentCount);
+        System.out.println(counter.atomicCount);
         System.out.println(counter.count);
     }
 }
